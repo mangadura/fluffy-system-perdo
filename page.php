53b25b341e2d1736559806485f2c62c5a0466653
mangadura <vitor.frohn@escola.pr.gov.br>
2025-10-21 12:15:04 -0300

    Add files via upload

---
+++ page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<body>
+   <div class="formulario">
+
+  <form>
+   <h3>Formulario</h3>
+   <input type="text" placeholder="login" id="login">
+   <input type="password" placeholder="senha" id="senha">
+    <input type="telephone" placeholder="telefone" id="telefone">
+    <input type="email" placeholder="email" id="email">
+    <input type="submit" onclick="logar">
+   </form>
+   </div>
+</body>
+</html>
